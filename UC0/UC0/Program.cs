fd2bdee14129222343f0d3bdcaa89492ee3e268b
unknown <hindhujhasridhar@gmail.com>
2021-11-25 14:37:09 +0530

    delete

--- UC0/UC0/Program.cs
+++ UC0/UC0/Program.cs
@@ -1,7 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("USE CASE1");
- Console.WriteLine("USE CASE2");
 
 
 Console.WriteLine("USE CASE88" +
